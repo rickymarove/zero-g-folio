922ad66ea039f08d9ede49cffbb967f2fd5b3b9a
richardmarove <sorrowprivategt@gmail.com>
2026-09-18 12:16:45 +0700

    add google verification and configure domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ricky.space',
+  site: 'https://rickyastro.pages.dev',
   integrations: [sitemap()],
   redirects: {
     '/about': {
